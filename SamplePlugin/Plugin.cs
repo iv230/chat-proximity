a786ab8d8bb3e4894af86eb2c6f214290583ae44
iv230 <lucas.kessayan@gmail.com>
2024-07-11 23:43:06 +0200

    First POC

--- SamplePlugin/Plugin.cs
+++ SamplePlugin/Plugin.cs
@@ -1,10 +1,21 @@
-﻿using Dalamud.Game.Command;
+using Dalamud.Game.Command;
 using Dalamud.IoC;
 using Dalamud.Plugin;
 using System.IO;
 using Dalamud.Interface.Windowing;
 using Dalamud.Plugin.Services;
 using SamplePlugin.Windows;
+using Dalamud.Game.Text;
+using Dalamud.Game.Text.SeStringHandling;
+using System;
+using Dalamud.Game.Text.SeStringHandling.Payloads;
+using System.Linq;
+using System.Collections.Generic;
+using Dalamud.Game.ClientState.Objects.SubKinds;
+using System.Numerics;
+using FFXIVClientStructs.FFXIV.Client.Game.Character;
+using FFXIVClientStructs.FFXIV.Client.Game.Control;
+using System.Diagnostics;
 
 namespace SamplePlugin;
 
@@ -13,17 +24,22 @@ public sealed class Plugin : IDalamudPlugin
     [PluginService] internal static IDalamudPluginInterface PluginInterface { get; private set; } = null!;
     [PluginService] internal static ITextureProvider TextureProvider { get; private set; } = null!;
     [PluginService] internal static ICommandManager CommandManager { get; private set; } = null!;
+    [PluginService] internal static IChatGui ChatGui { get; private set; } = null!;
+    [PluginService] internal static IPluginLog PluginLog { get; private set; } = null!;
+    [PluginService] internal static IObjectTable ObjectTable { get; private set; } = null!;
+    [PluginService] internal static IClientState ClientState { get; private set; } = null!;
 
     private const string CommandName = "/pmycommand";
 
     public Configuration Configuration { get; init; }
 
-    public readonly WindowSystem WindowSystem = new("SamplePlugin");
+    public readonly WindowSystem WindowSystem = new("ChatProximity");
     private ConfigWindow ConfigWindow { get; init; }
     private MainWindow MainWindow { get; init; }
 
     public Plugin()
     {
+        PluginLog.Info("Starting Chat Proximity!");
         Configuration = PluginInterface.GetPluginConfig() as Configuration ?? new Configuration();
 
         // you might normally want to embed resources and load them from the manifest stream
@@ -34,6 +50,8 @@ public sealed class Plugin : IDalamudPlugin
 
         WindowSystem.AddWindow(ConfigWindow);
         WindowSystem.AddWindow(MainWindow);
+
+        ChatGui.ChatMessage += HandleMessage;
 
         CommandManager.AddHandler(CommandName, new CommandInfo(OnCommand)
         {
@@ -52,12 +70,16 @@ public sealed class Plugin : IDalamudPlugin
 
     public void Dispose()
     {
+        PluginLog.Info("Disposing Chat Proximity");
+
         WindowSystem.RemoveAllWindows();
 
         ConfigWindow.Dispose();
         MainWindow.Dispose();
 
         CommandManager.RemoveHandler(CommandName);
+
+        ChatGui.ChatMessage -= HandleMessage;
     }
 
     private void OnCommand(string command, string args)
@@ -66,6 +88,59 @@ public sealed class Plugin : IDalamudPlugin
         ToggleMainUI();
     }
 
+    private unsafe void HandleMessage(XivChatType type, int timestamp, ref SeString sender, ref SeString message, ref bool isHandled)
+    {
+       if (type == XivChatType.Say)
+        {
+            PluginLog.Debug($"Caught {type} message from {sender.TextValue}: {message}");
+            try
+            {
+                if (isHandled)
+                {
+                    return;
+                }
+
+                List<Payload> finalPayload = [];
+
+                var currentPlayer = (BattleChara*)(ClientState.LocalPlayer?.Address ?? 0);
+
+                if (sender != null && currentPlayer != null)
+                {
+                    var senderCharacter = CharacterManager.Instance()->LookupBattleCharaByName(sender.ToString(), true);
+
+                    if (senderCharacter != null) {
+                        if (senderCharacter != currentPlayer)
+                        {
+                            PluginLog.Debug($"Found character: {senderCharacter->Name.ToString()}");
+                            var distance = Vector3.Distance(currentPlayer->Position, senderCharacter->Position);
+
+                            var colors = new List<UIForegroundPayload> { new(1), new(2), new(3), new(4), new(5), new(6) };
+                            var colorIndex = (int)distance * colors.Count / 20; // 20 is the max range of Say chat
+                            finalPayload.Add(colors[colorIndex]);
+
+                            PluginLog.Debug($"Computed distance: {distance}, index {colorIndex}");
+                        }
+                        else
+                        {
+                            PluginLog.Debug("Self message, no color change");
+                        }
+                    } else {
+                        PluginLog.Warning($"No sender character found: {sender}");
+                    }
+                }
+
+                finalPayload.Add(new TextPayload(message.TextValue));
+                message = new SeString(finalPayload);
+                PluginLog.Debug($"New message is: {message}");
+            }
+            catch (Exception e)
+            {
+                PluginLog.Error("Exception while processing message", e);
+            }
+            PluginLog.Debug("Exiting... =============");
+        }
+    }
+    
     private void DrawUI() => WindowSystem.Draw();
 
     public void ToggleConfigUI() => ConfigWindow.Toggle();
