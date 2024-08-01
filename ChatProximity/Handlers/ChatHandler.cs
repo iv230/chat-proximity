4be3d3bf53c43ccdd089e535fcf5da9223de34fd
iv230 <lucas.kessayan@gmail.com>
2024-08-02 00:29:29 +0200

    Added dirty message handling

--- ChatProximity/Handlers/ChatHandler.cs
+++ ChatProximity/Handlers/ChatHandler.cs
@@ -4,6 +4,7 @@ using Dalamud.Game.Text;
 using FFXIVClientStructs.FFXIV.Client.Game.Character;
 using System;
 using System.Collections.Generic;
+using FFXIVClientStructs.FFXIV.Common.Math;
 
 namespace ChatProximity.Handlers;
 
@@ -16,6 +17,13 @@ internal partial class ChatHandler(ChatProximityPlugin chatProximityPlugin)
     [System.Text.RegularExpressions.GeneratedRegex("[★●▲♦♥♠♣]")]
     private static partial System.Text.RegularExpressions.Regex FriendIconsRegex();
 
+    /// <summary>
+    /// Main message handling process
+    /// </summary>
+    /// <param name="type">Message type</param>
+    /// <param name="sender">Character who sent the message</param>
+    /// <param name="message">The message content</param>
+    /// <param name="isHandled">Whenever the message is handled or not</param>
     public unsafe void OnMessage(XivChatType type, ref SeString sender, ref SeString message, ref bool isHandled)
     {
         if (isHandled)
@@ -33,17 +41,13 @@ internal partial class ChatHandler(ChatProximityPlugin chatProximityPlugin)
         ChatProximityPlugin.PluginLog.Debug($"Caught {type} message from {GetPlayerNameForLog(sender.TextValue)}: {GetMessageForLog(message)}");
         try
         {
-            List<Payload> finalPayload = [];
-
             var currentPlayer = (BattleChara*)(ChatProximityPlugin.ClientState.LocalPlayer?.Address ?? 0);
-
             if (currentPlayer == null) {
                 ChatProximityPlugin.PluginLog.Warning("Current player is null");
                 return;
             }
 
             var senderCharacter = GetSender(sender);
-
             if (senderCharacter == null)
             {
                 ChatProximityPlugin.PluginLog.Warning($"Could not resolve sender character: {sender}");
@@ -57,20 +61,19 @@ internal partial class ChatHandler(ChatProximityPlugin chatProximityPlugin)
             }
 
             ChatProximityPlugin.PluginLog.Debug($"Found character: {GetPlayerNameForLog(senderCharacter->Name.ToString())}");
-            var distanceVector = currentPlayer->Position - senderCharacter->Position;
+            ChatProximityPlugin.PluginLog.Verbose($"Message is {message.ToJson()}");
 
-            if (ChatProximityPlugin.Configuration.VerticalIncrease)
+            var distance = GetDistance(currentPlayer->Position, senderCharacter->Position);
+            var colorKey = GetColor(distance);
+
+            if (IsMessageDirty(message))
             {
-                distanceVector.Y *= 2;   
+                HandleDirtyMessage(ref message, colorKey);
             }
-
-            var distance = distanceVector.Magnitude;
-
-            finalPayload.Add(GetColor(distance));
-            finalPayload.Add(new TextPayload(message.TextValue));
-            finalPayload.Add(UIForegroundPayload.UIForegroundOff);
-
-            message = new SeString(finalPayload);
+            else
+            {
+                HandleNotDirtyMessage(ref message, colorKey);
+            }
         }
         catch (Exception e)
         {
@@ -80,40 +83,127 @@ internal partial class ChatHandler(ChatProximityPlugin chatProximityPlugin)
         ChatProximityPlugin.PluginLog.Debug("Finished processing message");
     }
 
+    /// <summary>
+    /// Retrieve the sender object from its name
+    /// </summary>
+    /// <param name="sender">The sender name</param>
+    /// <returns>The sender object pointers</returns>
     private static unsafe BattleChara* GetSender(SeString sender)
     {
         var senderName = FriendIconsRegex().Replace(sender.ToString(), "");
         return CharacterManager.Instance()->LookupBattleCharaByName(senderName, true);
     }
-
-    private String GetPlayerNameForLog(String playerName)
+    
+    /// <summary>
+    /// Indicates is a message is already touched by Dalamud or another plugin
+    /// </summary>
+    /// <param name="message">The message to check</param>
+    /// <returns>A boolean indicating if the message is dirty or not</returns>
+    private static bool IsMessageDirty(SeString message)
     {
-        if (ChatProximityPlugin.Configuration.AnonymiseNames)
-        {
-            return playerName.Length > 2 ? playerName[..2] + "..." : playerName;
-        }
-
-        return playerName;
+        return message.Payloads.Count > 0 && message.Payloads[0] is UIForegroundPayload { IsEnabled: true };
     }
 
+    /// <summary>
+    /// Anonymise if needed a player name
+    /// </summary>
+    /// <param name="playerName">The player name to anonymise</param>
+    /// <returns>The two first characters of the name followed by ... if anonymise is enabled, the whole name otherwise</returns>
+    private String GetPlayerNameForLog(String playerName)
+    {
+        return ChatProximityPlugin.Configuration.AnonymiseNames && playerName.Length > 2
+                   ? playerName[..2] + "..."
+                   : playerName;
+    }
+
+    /// <summary>
+    /// Anonymise if needed a message
+    /// </summary>
+    /// <param name="message">The message to anonymise</param>
+    /// <returns>The two first characters of the message followed by ... if anonymise is enabled, the whole message otherwise</returns>
     private string GetMessageForLog(SeString message)
     {
         var messageText = message.ToString();
-        if (ChatProximityPlugin.Configuration.AnonymiseNames)
-        {
-            return messageText.Length > 2 ? messageText[..2] + "..." : messageText;
-        }
-
-        return messageText;
+        return ChatProximityPlugin.Configuration.AnonymiseNames && messageText.Length > 2
+                   ? messageText[..2] + "..."
+                   : messageText;
     }
 
-    private static UIForegroundPayload GetColor(float distance)
+    /// <summary>
+    /// Compute the distance between two players
+    /// Add a weight to the vertical distance if related config is enabled
+    /// </summary>
+    /// <param name="player1">The first player position</param>
+    /// <param name="player2">The second player position</param>
+    /// <returns>The distance between those two players</returns>
+    private float GetDistance(Vector3 player1, Vector3 player2)
     {
-        var colors = new List<UIForegroundPayload> { new(1), new(2), new(3), new(4), new(5) };
+        var distanceVector = player1 - player2;
+
+        if (ChatProximityPlugin.Configuration.VerticalIncrease)
+        {
+            distanceVector.Y *= 2;   
+        }
+            
+        return distanceVector.Magnitude;
+    }
+    
+    /// <summary>
+    /// Get the color according to distance
+    /// </summary>
+    /// <param name="distance">The distance between the two players</param>
+    /// <returns>A UI payload containing the color</returns>
+    private static ushort GetColor(float distance)
+    {
+        var colors = new List<ushort> { 1, 2, 3, 4, 5 };
         var colorIndex = (int)(distance * colors.Count / SayRange);
         colorIndex = Math.Clamp(colorIndex, 0, colors.Count - 1);  // Ensure index is within bounds
 
         ChatProximityPlugin.PluginLog.Debug($"Computed distance: {distance}, index {colorIndex}");
         return colors[colorIndex];
+    }
+
+    /// <summary>
+    /// Handles a dirty message by modifying its payload
+    /// </summary>
+    /// <param name="message">The message to handle</param>
+    /// <param name="colorKey">The color of the message</param>
+    private void HandleDirtyMessage(ref SeString message, ushort colorKey)
+    {
+        ChatProximityPlugin.PluginLog.Debug("Message dirty");
+
+        // Adding a first element to color the first chunk
+        if (message.Payloads[0] is UIForegroundPayload { IsEnabled: false })
+        {
+            message.Payloads.Insert(0, new UIForegroundPayload(colorKey));
+        }
+
+        // Then process all other chunks
+        foreach (var payload in message.Payloads)
+        {
+            if (payload is UIForegroundPayload { IsEnabled: false } currentForegroundPayload)
+            {
+                currentForegroundPayload.ColorKey = colorKey;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Handles a not dirty message by building a new payload
+    /// </summary>
+    /// <param name="message">The message to handle</param>
+    /// <param name="colorKey">The color of the message</param>
+    private void HandleNotDirtyMessage(ref SeString message, ushort colorKey)
+    {
+        ChatProximityPlugin.PluginLog.Debug("Message not dirty");
+        List<Payload> finalPayload =
+        [
+            new UIForegroundPayload(colorKey),
+            new TextPayload(message.TextValue),
+            UIForegroundPayload.UIForegroundOff
+
+        ];
+
+        message = new SeString(finalPayload);
     }
 }
