b5a449df8f9097ca080e18b6aab74c8dad53f85d
iv230 <lucas.kessayan@gmail.com>
2024-08-20 18:46:57 +0200

    Fixed configuration

--- ChatProximity/Config/ChatTypeConfig.cs
+++ ChatProximity/Config/ChatTypeConfig.cs
@@ -3,11 +3,10 @@ using Dalamud.Game.Text;
 
 namespace ChatProximity.Config;
 
-public class ChatTypeConfig(XivChatType type, bool enabled, Vector4 closestColor, Vector4 farthestColor)
+public class ChatTypeConfig(XivChatType type, bool enabled, Vector4 closestColor = new(), Vector4 farthestColor = new())
 {
     public XivChatType Type { get; set; } = type;
     public bool Enabled { get; set; } = enabled;
     public Vector4 ClosestColor { get; set; } = closestColor;
     public Vector4 FarthestColor { get; set; } = farthestColor;
-    
 }
--- ChatProximity/Config/Configuration.cs
+++ ChatProximity/Config/Configuration.cs
@@ -11,23 +11,13 @@ public class Configuration : IPluginConfiguration
 {
     public int Version { get; set; } = 0;
 
-    // public List<XivChatType> AffectedChatTypes = [XivChatType.Say, XivChatType.Shout, XivChatType.CustomEmote];
-
     public bool RecolorSayChat { get; set; } = true;
     public bool VerticalIncrease { get; set; } = true;
     public bool AnonymiseNames { get; set; } = true;
-    // public List<ChatTypeConfig> ChatTypeConfigs = [
-    //     new ChatTypeConfig(XivChatType.Say, true, new Color(255, 255, 255, 255), new Color(100, 100, 100, 255)),
-    //     new ChatTypeConfig(XivChatType.Shout, true, new Color(255, 255, 255, 255), new Color(100, 100, 100, 255)),
-    //     new ChatTypeConfig(XivChatType.CustomEmote, true, new Color(255, 255, 255, 255), new Color(100, 100, 100, 255)),
-    // ];
 
-    public ChatTypeConfig SayConfig { get; set; } =
-        new (XivChatType.Say, true, new Vector4(255, 255, 255, 255), new Vector4(100, 100, 100, 255));
-    public ChatTypeConfig ShoutConfig { get; set; } =
-        new (XivChatType.Shout, true, new Vector4(255, 255, 255, 255), new Vector4(100, 100, 100, 255));
-    public ChatTypeConfig EmoteConfig { get; set; } =
-        new (XivChatType.CustomEmote, true, new Vector4(255, 255, 255, 255), new Vector4(100, 100, 100, 255));
+    public ChatTypeConfig SayConfig { get; set; } = new(XivChatType.Say, true, new Vector4(1f, 1f, 1f, 1f), new Vector4(0.3f, 0.3f, 0.3f, 1f));
+    public ChatTypeConfig ShoutConfig { get; set; } = new(XivChatType.Shout, true, new Vector4(1f, 0.85f, 0.01f, 1f), new Vector4(0.29f, 0.25f, 0.11f, 1f));
+    public ChatTypeConfig EmoteConfig { get; set; } = new (XivChatType.CustomEmote, true, new Vector4(0.72f, 1f, 0.94f, 1f), new Vector4(0.21f, 0.29f, 0.28f, 1f));
 
     public void Save()
     {
