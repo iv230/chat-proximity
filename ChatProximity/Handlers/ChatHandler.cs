34fc4905d7cc1fd44f3d52f3170c4fc6ff5022b1
iv230 <lucas.kessayan@gmail.com>
2024-12-07 16:55:40 +0100

    Added threshold to make messages disappear

--- ChatProximity/Handlers/ChatHandler.cs
+++ ChatProximity/Handlers/ChatHandler.cs
@@ -1,14 +1,12 @@
-using Dalamud.Game.Text;
-using FFXIVClientStructs.FFXIV.Client.Game.Character;
 using System;
 using ChatProximity.Config;
+using ChatProximity.Service;
+using Dalamud.Game.Text;
 using Dalamud.Game.Text.SeStringHandling.Payloads;
-using Dalamud.Utility;
 using FFXIVClientStructs.FFXIV.Client.Game;
+using FFXIVClientStructs.FFXIV.Client.Game.Character;
 using FFXIVClientStructs.FFXIV.Common.Math;
-using Lumina.Text.ReadOnly;
 using SeString = Dalamud.Game.Text.SeStringHandling.SeString;
-using SeStringBuilder = Lumina.Text.SeStringBuilder;
 
 namespace ChatProximity.Handlers;
 
@@ -27,10 +25,10 @@ internal class ChatHandler(ChatProximity plugin)
     {
         if (isHandled)
         {
-            ChatProximity.Log.Verbose("Message considered has handled, abort");
+            ChatProximity.Log.Verbose("Message considered as handled, abort");
             return;
         }
-        
+
         Plugin.Configuration.ChatTypeConfigs.TryGetValue(type, out var config);
 
         if (config is not { Enabled: true })
@@ -62,8 +60,19 @@ internal class ChatHandler(ChatProximity plugin)
                 return;
             }
 
+            float? distance = GetDistance(currentCharacter->Position, senderCharacter->Position);
+
+            if (Math.Abs(config.Range - config.Threshold) > 0.001f && distance > config.Threshold)
+            {
+                ChatProximity.Log.Info($"Messaged dropped: [{type.ToString()}] {senderCharacter->NameString} : {message.TextValue}");
+                ChatProximity.Log.Debug($"Distance {distance} exceeds threshold {config.Threshold}");
+                ChatMessageService.DropMessage(ref message, ref isHandled);
+                return;
+            }
+            
+            ChatProximity.Log.Debug($"Found character: {GetPlayerNameForLog(senderCharacter->NameString)}");
+
             RecolorMode recolorMode;
-            float? distance = null;
 
             if (Plugin.Configuration.RecolorTargeted &&
                 currentCharacter->EntityId == senderCharacter->GetTargetId().ObjectId)
@@ -78,16 +87,10 @@ internal class ChatHandler(ChatProximity plugin)
             else
             {
                 recolorMode = RecolorMode.Distance;
-                distance = GetDistance(currentCharacter->Position, senderCharacter->Position);
             }
 
-            ChatProximity.Log.Debug($"Found character: {GetPlayerNameForLog(senderCharacter->NameString)}");
-            ChatProximity.Log.Verbose($"Message is {message.ToJson()}");
-
             var colorKey = GetColor(recolorMode, config, distance);
-            HandleMessage(ref message, colorKey);
-
-            ChatProximity.Log.Verbose($"New message is {message.ToJson()}");
+            ChatMessageService.HandleMessage(ref message, colorKey);
         }
         catch (Exception e)
         {
@@ -213,43 +216,6 @@ internal class ChatHandler(ChatProximity plugin)
             default:
                 throw new ArgumentOutOfRangeException(nameof(mode), mode, null);
         }
-    }
-
-    /// <summary>
-    /// Handles a dirty message by modifying its payload
-    /// </summary>
-    /// <param name="message">The message to handle</param>
-    /// <param name="color">The computed color</param>
-    private static void HandleMessage(ref SeString message, Vector4 color)
-    {
-        var sb = new SeStringBuilder();
-
-        // Extracting and processing payloads
-        for (var i = 0; i < message.Payloads.Count; i++)
-        {
-            var payload = message.Payloads[i];
-
-            if (payload is TextPayload textPayload)
-            {
-                ChatProximity.Log.Verbose($"i = {i}");
-                var previousPayLoad = i > 0 ? message.Payloads[i - 1] : null;
-
-                if (previousPayLoad is UIForegroundPayload { IsEnabled: true } previousPayload)
-                    sb.PushColorType(previousPayload.ColorKey);
-                else
-                    sb.PushColorRgba((byte)(color.X*255), (byte)(color.Y*255), (byte)(color.Z*255), (byte)(color.W*255));
-
-                sb.Append(textPayload.Text);
-                sb.PopColor();
-            }
-            else if (payload is not UIForegroundPayload)
-            {
-                sb.Append(new ReadOnlySeStringSpan(payload.Encode()));
-            }
-        }
-
-        // Update the message with the new payloads
-        message = sb.ToSeString().ToDalamudString();
     }
 
     /// <summary>
--- ChatProximity/Windows/ConfigWindow.cs
+++ ChatProximity/Windows/ConfigWindow.cs
@@ -25,6 +25,9 @@ public class ConfigWindow : Window, IDisposable
 
     public override void PreDraw() { }
 
+    /// <summary>
+    /// Draws the whole window
+    /// </summary>
     public override void Draw()
     {
         ImGui.TextUnformatted("General Settings");
@@ -39,6 +42,9 @@ public class ConfigWindow : Window, IDisposable
         DrawChatTable();
     }
 
+    /// <summary>
+    /// Draws a list of checkboxes representing general plugin config
+    /// </summary>
     private void DrawGeneralConfig()
     {
         var verticalIncrease = configuration.VerticalIncrease;
@@ -47,7 +53,9 @@ public class ConfigWindow : Window, IDisposable
             configuration.VerticalIncrease = verticalIncrease;
             configuration.Save();
         }
-        DrawTooltip("(?)", "Enabling this setting increases the importance of vertical distance when calculating proximity. Vertical distances will weigh more heavily when determining chat range, which can be useful in multi-floor or elevated areas.");
+        DrawTooltip("(?)", "Enabling this setting increases the importance of vertical distance when calculating" +
+                           " proximity. Vertical distances will weigh more heavily when determining chat range, which" +
+                           " can be useful in multi-floor or elevated areas.");
 
         var insideReducer = configuration.InsideReducer;
         if (ImGui.Checkbox("Reduce the chat range while inside a housing", ref insideReducer))
@@ -55,7 +63,8 @@ public class ConfigWindow : Window, IDisposable
             configuration.InsideReducer = insideReducer;
             configuration.Save();
         }
-        DrawTooltip("(?)", "When enabled, this option reduces the effective chat range while inside a house or apartment, allowing for more immersive communication by adjusting distance calculations.");
+        DrawTooltip("(?)", "When enabled, this option reduces the effective chat range while inside a house or" +
+                           " apartment, allowing for more immersive communication by adjusting distance calculations.");
 
         var recolorTargeting = configuration.RecolorTargeting;
         if (ImGui.Checkbox("Recolor message when targeting sender", ref recolorTargeting))
@@ -63,7 +72,8 @@ public class ConfigWindow : Window, IDisposable
             configuration.RecolorTargeting = recolorTargeting;
             configuration.Save();
         }
-        DrawTooltip("(?)", "When enabled, messages sent by a player you are targeting will have a specific color, allowing better identification of their messages.");
+        DrawTooltip("(?)", "When enabled, messages sent by a player you are targeting will have a specific color," +
+                           " allowing better identification of their messages.");
 
         var recolorTargeted = configuration.RecolorTargeted;
         if (ImGui.Checkbox("Recolor message when sender targets you", ref recolorTargeted))
@@ -71,7 +81,27 @@ public class ConfigWindow : Window, IDisposable
             configuration.RecolorTargeted = recolorTargeted;
             configuration.Save();
         }
-        DrawTooltip("(?)", "When enabled, messages from a player targeting you will have a specific color, making them easier to identify.");
+        DrawTooltip("(?)", "When enabled, messages from a player targeting you will have a specific color, making" +
+                           " them easier to identify.");
+        
+        var editThreshold = configuration.EditThreshold;
+        if (ImGui.Checkbox("Edit Threshold (Dangerous)", ref editThreshold))
+        {
+            configuration.EditThreshold = editThreshold;
+
+            if (!editThreshold)
+            {
+                configuration.ResetAllThreshold();
+            }
+            
+            configuration.Save();
+        }
+        DrawTooltip("(?)", "Enabling this option adds a 'Threshold' column to the chat configuration table." +
+                           " This column allows you to set a maximum distance for each channel, beyond which messages" +
+                           " will be entirely suppressed and not displayed. Warning: Messages from a distance greater" +
+                           " than the specified threshold will be permanently deleted and cannot be recovered." +
+                           " Use this option with caution, as it can result in the loss of important communications," +
+                           " especially in role play.");
 
         var anonymise = configuration.AnonymiseNames;
         if (ImGui.Checkbox("Anonymise player names in logs", ref anonymise))
@@ -79,15 +109,20 @@ public class ConfigWindow : Window, IDisposable
             configuration.AnonymiseNames = anonymise;
             configuration.Save();
         }
-        DrawTooltip("(?)", "When this option is enabled, player names will be anonymized in Dalamud's debug logs. This is useful for protecting privacy in logs created for plugin debugging or troubleshooting.");
+        DrawTooltip("(?)", "When this option is enabled, player names will be anonymized in Dalamud's debug logs." +
+                           " This is useful for protecting privacy in logs created for plugin debugging or troubleshooting.");
     }
 
+    /// <summary>
+    /// Draws the table handling config for each chat type
+    /// </summary>
     private void DrawChatTable()
     {
         var columnCount = 4;
 
         if (configuration.RecolorTargeting) columnCount++;
         if (configuration.RecolorTargeted) columnCount++;
+        if (configuration.EditThreshold) columnCount++;
 
         if (ImGui.BeginTable("ChatTypesTable##", columnCount))
         {
@@ -102,14 +137,24 @@ public class ConfigWindow : Window, IDisposable
             if (configuration.RecolorTargeted)
                 ImGui.TableSetupColumn("Targeted Color", ImGuiTableColumnFlags.NoHide | ImGuiTableColumnFlags.WidthFixed);
 
+            ImGui.TableSetupColumn("Threshold", ImGuiTableColumnFlags.NoHide | ImGuiTableColumnFlags.WidthFixed);
+
             ImGui.TableHeadersRow();
 
-            ImGuiClip.ClippedDraw(configuration.ChatTypeConfigs.Values.ToList(), DrawChatTableLine, ImGui.GetTextLineHeight() + (3 * ImGui.GetStyle().FramePadding.Y));
+            ImGuiClip.ClippedDraw(
+                configuration.ChatTypeConfigs.Values.ToList(),
+                DrawChatTableLine,
+                ImGui.GetTextLineHeight() + (3 * ImGui.GetStyle().FramePadding.Y)
+            );
 
             ImGui.EndTable();
         }
     }
 
+    /// <summary>
+    /// Draws the line for a chat type config
+    /// </summary>
+    /// <param name="chatTypeConfig">The config to draw line for</param>
     private void DrawChatTableLine(ChatTypeConfig chatTypeConfig)
     {
         using var id = ImRaii.PushId(chatTypeConfig.Type.ToString());
@@ -167,8 +212,63 @@ public class ConfigWindow : Window, IDisposable
                 configuration.Save();
             }
         }
+
+        if (configuration.EditThreshold)
+        {
+            DrawThresholdCell(chatTypeConfig);
+        }
     }
 
+    /// <summary>
+    /// Draws the cell containing the slider for editing the threshold
+    /// </summary>
+    /// <param name="config">The concerned chat type</param>
+    private void DrawThresholdCell(ChatTypeConfig config)
+    {
+        var min = config.Range * 0.1f;
+        var max = config.Range;
+        var threshold = config.Threshold;
+
+        ImGui.TableNextColumn();
+        ImGui.SetNextItemWidth(100);
+
+        if (ImGui.SliderFloat("##threshold", ref threshold, min, max, "%.1f"))
+        {
+            config.Threshold = Math.Clamp(threshold, min, max);
+            configuration.Save();
+        }
+
+        if (ImGui.IsItemHovered())
+        {
+            ImGui.SetTooltip("Right-click to manually enter a precise value.");
+        }
+
+        if (ImGui.IsItemHovered() && ImGui.IsMouseClicked(ImGuiMouseButton.Right))
+        {
+            ImGui.OpenPopup("EditThresholdPopup");
+        }
+
+        if (ImGui.BeginPopup("EditThresholdPopup"))
+        {
+            ImGui.SetNextItemWidth(100);
+            if (ImGui.InputFloat("Edit Threshold##input", ref threshold, 0.1f, 1.0f, "%.1f"))
+            {
+                config.Threshold = Math.Clamp(threshold, min, max);
+                configuration.Save();
+            }
+            if (ImGui.Button("Close"))
+            {
+                ImGui.CloseCurrentPopup();
+            }
+            ImGui.EndPopup();
+        }
+    }
+
+    /// <summary>
+    /// Draws a tooltip element
+    /// </summary>
+    /// <param name="tooltip">The text that will show the tooltip if hovered</param>
+    /// <param name="text">The tooltip content</param>
     private static void DrawTooltip(String tooltip, String text)
     {
         ImGui.SameLine();
